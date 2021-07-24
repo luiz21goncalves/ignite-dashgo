6fec4da449bff0fc15464202af60a0bdb82d4308
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2021-07-24 17:33:57 -0300

    feat: add responsivity to Profile component

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Luiz Gonçalves</Text>
-        <Text color="gray.300" fontSize="small">
-          luizhbgoncalves@gmail.com
-        </Text>
-      </Box>
+      { showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Luiz Gonçalves</Text>
+          <Text color="gray.300" fontSize="small">
+            luizhbgoncalves@gmail.com
+          </Text>
+        </Box>
+      ) }
 
       <Avatar size="md" name="Luiz Gonçalves" src="https://github.com/luiz21goncalves.png"/>
     </Flex>
