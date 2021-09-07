f3d77ef02ba04dc640505185622c3a946b97a1a8
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2021-09-07 10:29:32 -0300

    feat: add react-query config

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -12,16 +12,25 @@ import {
   Th,
   Thead,
   Tr,
-  useBreakpointValue
+  useBreakpointValue,
+  Spinner,
 } from "@chakra-ui/react";
 import Link from "next/link";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { useQuery } from "react-query";
 
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
+  const { data, isLoading, error } = useQuery('users', async () => {
+    const response = await fetch('http://localhost:3000/api/users')
+    const data = await response.json()
+
+    return data
+  })
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
@@ -53,112 +62,125 @@ export default function UserList() {
             </Link>
           </Flex>
 
-          <Table colorScheme="whiteAlpha">
-            <Thead>
-              <Tr>
-                <Th px={["4", "4", "6"]} color="gray.300" w="8" >
-                  <Checkbox colorScheme="pink" />
-                </Th>
+          { isLoading ? (
+            <Flex justify="center">
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex justify="center">
+              <Text>Falha ao obter os dados dos usuários</Text>
+            </Flex>
+          ) : (
+          <>
+            <Table colorScheme="whiteAlpha">
+              <Thead>
+                <Tr>
+                  <Th px={["4", "4", "6"]} color="gray.300" w="8" >
+                    <Checkbox colorScheme="pink" />
+                  </Th>
 
-                <Th>Usuário</Th>
+                  <Th>Usuário</Th>
 
-                { isWideVersion && <Th>Data de cadastro</Th> }
+                  { isWideVersion && <Th>Data de cadastro</Th> }
 
-                { isWideVersion && <Th w="8"/> }
-              </Tr>
-            </Thead>
+                  { isWideVersion && <Th w="8"/> }
+                </Tr>
+              </Thead>
 
-            <Tbody>
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-
-                <Td>
-                  <Text fontWeight="bold">Luiz Gonçalves</Text>
-                  <Text fontSize="sm" color="gray.300">
-                    luizhbgoncalves@gmail.com
-                  </Text>
-                </Td>
-
-                { isWideVersion && <Td>04 de abril, 2021</Td> }
-                
-                { isWideVersion && (
-                  <Td>
-                    <Button 
-                      as="a"
-                      size="sm"
-                      fontSize="sm"
-                      colorScheme="purple"
-                      leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                    >
-                      Editar
-                    </Button>
+              <Tbody>
+                <Tr>
+                  <Td px={["4", "4", "6"]}>
+                    <Checkbox colorScheme="pink" />
                   </Td>
-                ) }
-              </Tr>
 
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-
-                <Td>
-                  <Text fontWeight="bold">Luiz Gonçalves</Text>
-                  <Text fontSize="sm" color="gray.300">
-                    luizhbgoncalves@gmail.com
-                  </Text>
-                </Td>
-
-                { isWideVersion && <Td>04 de abril, 2021</Td> }
-                
-                { isWideVersion && (
                   <Td>
-                    <Button 
-                      as="a"
-                      size="sm"
-                      fontSize="sm"
-                      colorScheme="purple"
-                      leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                    >
-                      Editar
-                    </Button>
+                    <Text fontWeight="bold">Luiz Gonçalves</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      luizhbgoncalves@gmail.com
+                    </Text>
                   </Td>
-                ) }
-              </Tr>
 
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
+                  { isWideVersion && <Td>04 de abril, 2021</Td> }
+                  
+                  { isWideVersion && (
+                    <Td>
+                      <Button 
+                        as="a"
+                        size="sm"
+                        fontSize="sm"
+                        colorScheme="purple"
+                        leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                      >
+                        Editar
+                      </Button>
+                    </Td>
+                  ) }
+                </Tr>
 
-                <Td>
-                  <Text fontWeight="bold">Luiz Gonçalves</Text>
-                  <Text fontSize="sm" color="gray.300">
-                    luizhbgoncalves@gmail.com
-                  </Text>
-                </Td>
+                <Tr>
+                  <Td px={["4", "4", "6"]}>
+                    <Checkbox colorScheme="pink" />
+                  </Td>
 
-                { isWideVersion && <Td>04 de abril, 2021</Td> }
-                
-                { isWideVersion && (
                   <Td>
-                    <Button 
-                      as="a"
-                      size="sm"
-                      fontSize="sm"
-                      colorScheme="purple"
-                      leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                    >
-                      Editar
-                    </Button>
+                    <Text fontWeight="bold">Luiz Gonçalves</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      luizhbgoncalves@gmail.com
+                    </Text>
                   </Td>
-                ) }
-              </Tr>
-            </Tbody>
-          </Table>
 
-          <Pagination />
+                  { isWideVersion && <Td>04 de abril, 2021</Td> }
+                  
+                  { isWideVersion && (
+                    <Td>
+                      <Button 
+                        as="a"
+                        size="sm"
+                        fontSize="sm"
+                        colorScheme="purple"
+                        leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                      >
+                        Editar
+                      </Button>
+                    </Td>
+                  ) }
+                </Tr>
+
+                <Tr>
+                  <Td px={["4", "4", "6"]}>
+                    <Checkbox colorScheme="pink" />
+                  </Td>
+
+                  <Td>
+                    <Text fontWeight="bold">Luiz Gonçalves</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      luizhbgoncalves@gmail.com
+                    </Text>
+                  </Td>
+
+                  { isWideVersion && <Td>04 de abril, 2021</Td> }
+                  
+                  { isWideVersion && (
+                    <Td>
+                      <Button 
+                        as="a"
+                        size="sm"
+                        fontSize="sm"
+                        colorScheme="purple"
+                        leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                      >
+                        Editar
+                      </Button>
+                    </Td>
+                  ) }
+                </Tr>
+              </Tbody>
+            </Table>
+
+            <Pagination />
+          </>
+          )}
+
         </Box>
       </Flex>
     </Box>
